5d91dd890ca12c85ac373973830a8fc710409d99
Kam <KamronVaswani@gmail.com>
2025-10-28 21:58:31 -0400

    Fix chat wrap mixin to capture trim formatting

--- src/main/java/kamkeel/hextext/mixins/early/impl/client/MixinGuiNewChat.java
+++ src/main/java/kamkeel/hextext/mixins/early/impl/client/MixinGuiNewChat.java
@@ -1,12 +1,15 @@
 package kamkeel.hextext.mixins.early.impl.client;
 
-import net.minecraft.client.gui.GuiNewChat;
 import kamkeel.hextext.util.StringUtils;
+import net.minecraft.client.gui.GuiNewChat;
+import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(GuiNewChat.class)
 public abstract class MixinGuiNewChat {
@@ -14,10 +17,9 @@ public abstract class MixinGuiNewChat {
     @Unique
     private String hextext$wrappedFormatPrefix;
 
-    @Redirect(method = "func_146237_a", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;getFormatFromString(Ljava/lang/String;)Ljava/lang/String;"))
-    private String hextext$captureWrappedPrefix(String original) {
-        hextext$wrappedFormatPrefix = StringUtils.extractFormatFromString(original);
-        return hextext$wrappedFormatPrefix;
+    @Inject(method = "func_146237_a", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;trimStringToWidth(Ljava/lang/String;IZ)Ljava/lang/String;", ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void hextext$captureWrappedPrefix(IChatComponent component, int lineId, int updateCounter, boolean refresh, CallbackInfo ci, int k, int l, net.minecraft.util.ChatComponentText chatcomponenttext, java.util.ArrayList arraylist, java.util.ArrayList arraylist1, int i1, IChatComponent ichatcomponent1, String s, int j1, net.minecraft.util.ChatComponentText chatcomponenttext1, boolean flag1, String s1) {
+        hextext$wrappedFormatPrefix = StringUtils.extractFormatFromString(s1);
     }
 
     @ModifyVariable(method = "func_146237_a",
