5e884a2f18dfc6189561312c3a283c7a3e46b008
Kam <KamronGaming@gmail.com>
2025-10-29 18:23:42 -0400

    Update MixinBlockSign.java

--- src/main/java/kamkeel/hextext/mixin/early/impl/MixinBlockSign.java
+++ src/main/java/kamkeel/hextext/mixin/early/impl/MixinBlockSign.java
@@ -3,7 +3,9 @@ package kamkeel.hextext.mixin.early.impl;
 import kamkeel.hextext.common.sign.SignSide;
 import kamkeel.hextext.common.sign.SignSideHelper;
 import kamkeel.hextext.common.sign.SignState;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockSign;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -17,72 +19,73 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockSign.class)
-public abstract class MixinBlockSign {
+public abstract class MixinBlockSign extends BlockContainer {
 
-    @Inject(method = "onBlockActivated", at = @At("HEAD"), cancellable = true)
-    private void hextext$onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side,
-            float hitX, float hitY, float hitZ, CallbackInfoReturnable<Boolean> cir) {
+
+    protected MixinBlockSign(Material p_i45386_1_) {
+        super(p_i45386_1_);
+    }
+
+    @Unique
+    public boolean onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX, float subY, float subZ)
+    {
         if (player == null || player.isSneaking()) {
-            return;
+            return false;
         }
 
-        TileEntity tileEntity = world.getTileEntity(x, y, z);
+        TileEntity tileEntity = worldIn.getTileEntity(x, y, z);
         if (!(tileEntity instanceof TileEntitySign)) {
-            return;
+            return false;
         }
 
         TileEntitySign sign = (TileEntitySign) tileEntity;
         SignState signState = (SignState) sign;
 
         if (signState.hextext$isWaxed()) {
-            cir.setReturnValue(true);
-            return;
+            return true;
         }
 
         SignSide clickedSide = SignSideHelper.determineSide(sign, player.posX, player.posZ);
         ItemStack stack = player.getCurrentEquippedItem();
 
         if (stack != null) {
-            if (world.isRemote) {
-                cir.setReturnValue(true);
-                return;
+            if (worldIn.isRemote) {
+                return true;
             }
 
             if (stack.getItem() == Items.glowstone_dust) {
                 if (signState.hextext$setGlowing(clickedSide, true)) {
                     hextext$consumeItem(player, stack);
-                    world.markBlockForUpdate(x, y, z);
+                    worldIn.markBlockForUpdate(x, y, z);
                 }
-                cir.setReturnValue(true);
-                return;
+                return true;
             }
 
             if (stack.getItem() == Items.dye && stack.getItemDamage() == 0) {
                 if (signState.hextext$setGlowing(clickedSide, false)) {
                     hextext$consumeItem(player, stack);
-                    world.markBlockForUpdate(x, y, z);
+                    worldIn.markBlockForUpdate(x, y, z);
                 }
-                cir.setReturnValue(true);
-                return;
+                return true;
             }
 
             if (stack.getItem() == Items.slime_ball) {
                 if (!signState.hextext$isWaxed()) {
                     signState.hextext$setWaxed(true);
                     hextext$consumeItem(player, stack);
-                    world.markBlockForUpdate(x, y, z);
+                    worldIn.markBlockForUpdate(x, y, z);
                 }
-                cir.setReturnValue(true);
+                return true;
             }
-            return;
         }
 
         signState.hextext$prepareForEdit(clickedSide);
         sign.func_145912_a(player);
-        if (!world.isRemote) {
+        if (!worldIn.isRemote) {
             player.func_146100_a(sign);
         }
-        cir.setReturnValue(true);
+
+        return true;
     }
 
     @Unique
