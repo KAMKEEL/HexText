9650fee55a48291c13acd038c658e225751405ff
Kam <KamronVaswani@gmail.com>
2025-10-29 13:46:40 -0400

    Tune glowing outline color calculation

---
+++ src/main/java/kamkeel/hextext/client/render/font/GlowingTextRenderer.java
@@ -0,0 +1,48 @@
+package kamkeel.hextext.client.render.font;
+
+import kamkeel.hextext.common.util.ColorMath;
+
+/**
+ * Utilities for HexText's glowing text outline rendering.
+ */
+public final class GlowingTextRenderer {
+
+    private static final float[][] OUTLINE_OFFSETS = new float[][] {
+        {-1.0f, 0.0f},
+        {1.0f, 0.0f},
+        {0.0f, -1.0f},
+        {0.0f, 1.0f},
+        {-1.0f, -1.0f},
+        {-1.0f, 1.0f},
+        {1.0f, -1.0f},
+        {1.0f, 1.0f}
+    };
+
+    private static final float OUTLINE_DARKEN_FACTOR = 0.4f;
+    private static final float OUTLINE_RECOVERY_FACTOR = 0.35f;
+
+    private static boolean outlineEnabled = true;
+
+    private GlowingTextRenderer() {
+    }
+
+    public static boolean isOutlineEnabled() {
+        return outlineEnabled;
+    }
+
+    public static void setOutlineEnabled(boolean enabled) {
+        outlineEnabled = enabled;
+    }
+
+    public static float[][] getOutlineOffsets() {
+        return OUTLINE_OFFSETS;
+    }
+
+    public static int computeOutlineColor(int baseColor) {
+        int darkened = ColorMath.scaleBrightness(baseColor, OUTLINE_DARKEN_FACTOR);
+        if ((darkened & 0xFFFFFF) == 0 && (baseColor & 0xFFFFFF) != 0) {
+            return ColorMath.scaleBrightness(baseColor, OUTLINE_RECOVERY_FACTOR);
+        }
+        return darkened;
+    }
+}
