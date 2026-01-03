50f779f5e51f373042427c434a3df666cb41f38d
Kam <KamronGaming@gmail.com>
2026-01-03 01:22:55 -0500

    1.3 Hex Text

--- src/main/java/kamkeel/hextext/HexText.java
+++ src/main/java/kamkeel/hextext/HexText.java
@@ -21,7 +21,7 @@ public class HexText {
 
     public static final String NAME = "HexText";
     public static final String ID = "hextext";
-    public static final String VERSION = "1.3-beta";
+    public static final String VERSION = "1.3";
     @SidedProxy(clientSide = "kamkeel.hextext.client.ClientProxy", serverSide = "kamkeel.hextext.CommonProxy")
     public static CommonProxy proxy;
 
